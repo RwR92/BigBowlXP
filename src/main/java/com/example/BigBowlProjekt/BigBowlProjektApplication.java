3a53425ce81fb2a8c13b4973c4dd92fd39a96de1
magh0003 <magh0003@stud.ek.dk>
2026-09-23 09:22:53 +0200

    first

---
+++ src/main/java/com/example/BigBowlProjekt/BigBowlProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.BigBowlProjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigBowlProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigBowlProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BigBowlProjekt/BigBowlProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BigBowlProjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigBowlProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
