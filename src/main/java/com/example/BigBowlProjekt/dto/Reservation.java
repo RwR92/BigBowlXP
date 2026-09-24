441247969ea9ceace505a77d2c2c7d6162b9e202
loegcas <loegcas@gmail.com>
2026-09-24 10:07:49 +0200

    fixed Reservation dto

--- src/main/java/com/example/BigBowlProjekt/dto/Reservation.java
+++ src/main/java/com/example/BigBowlProjekt/dto/Reservation.java
@@ -3,13 +3,14 @@ package com.example.BigBowlProjekt.dto;
 import com.example.BigBowlProjekt.model.Lane;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record Reservation(
     Long id,
     List<Lane> lanes,
-    LocalDate startTime,
-    LocalDate endTime,
+    LocalDateTime startTime,
+    LocalDateTime endTime,
     Integer guest
 ) {
 }
